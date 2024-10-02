5b38f0334c0f7c51f0c74b0b2468f9648d39e65e
Ajeet Singh <ajeet.cloud@gmail.com>
2024-10-02 17:28:36 +0530

    Project setup

---
+++ src/main/java/com/example/springaimilvus/SpringAiMilvusApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaimilvus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMilvusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMilvusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springaimilvus/SpringAiMilvusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaimilvus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMilvusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
