e4f4979065fde95950d23cdfe672697fd4d43d19
Ajeet Singh <ajeet.cloud@gmail.com>
2024-10-27 20:00:56 +0530

    Added search functionality

--- src/main/java/com/example/springaimilvus/controller/ProductController.java
+++ src/main/java/com/example/springaimilvus/controller/ProductController.java
@@ -21,10 +21,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+
 import java.util.stream.IntStream;
 
 @RestController
